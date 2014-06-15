fada91349542aed80626246d58627051f671c68a
michelwin <michel.win@gmail.com>
2014-06-15 20:28:21 -0300

    WebForms MVC

---
+++ MVC/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+          
+            if(this.HttpContext.Session["name"] ==null)
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            else
+                ViewBag.Message = "Welcome to ASP.NET MVC! " + this.HttpContext.Session["name"].ToString();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
